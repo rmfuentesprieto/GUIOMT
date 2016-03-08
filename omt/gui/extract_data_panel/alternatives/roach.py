38cb983ccef21a0d26137bb0dc6b161a3242528a
amermelao <amermelao@gmail.com>
2016-03-08 16:45:56 -0300

    8/3/16

--- omt/gui/extract_data_panel/alternatives/roach.py
+++ omt/gui/extract_data_panel/alternatives/roach.py
@@ -36,25 +36,26 @@ class ROACH(Empty):
         new_reg.bind(on_press=self.add_registers)
 
         roach_register.add_widget(new_reg)
-
-        self.reg_container = GridLayout(cols=1, spacing = 1,size_hint=(1,None), size=(1,30))
+        ##### Regiter container
+        self.reg_container = GridLayout(cols=1, spacing = 1, size_hint_y=None)#row_default_height=30)
         self.reg_container.bind(minimum_height=self.reg_container.setter('height'))
 
-        scroll_root = ScrollView(size_hint=(1,None), size=(1,100))
+        scroll_root = ScrollView(size_hint=(None,None), height=100)
         scroll_root.add_widget(self.reg_container)
+        ####
 
-        # acc layout
         free_running = BoxLayout(orientation='horizontal', size_hint=(1,None), size=(1,30))
         add_free_running = Button(text = 'Adquirir Nuevo')
         add_free_running.bind(on_press=self.add_free_running)
-
         free_running.add_widget(add_free_running)
 
+        #### free run container
         self.free_run_container = GridLayout(cols=1, spacing = 1,size_hint=(1,None), size=(1,30))
         self.free_run_container.bind(minimum_height=self.free_run_container.setter('height'))
 
         scroll_root_free_run = ScrollView(size_hint=(1,None), size=(1,100))
         scroll_root_free_run.add_widget(self.free_run_container)
+        ####
 
         big_one.add_widget(roach_connection_info)
         big_one.add_widget(roach_register)
@@ -67,11 +68,12 @@ class ROACH(Empty):
         self.register_list = []
 
     def add_registers(self, instance):
-        btn = Register(orientation='horizontal')
-        self.reg_container.add_widget(btn)
+        data = Register(orientation='horizontal', size_hint_y=None, size=(1,30))
+        data.config()
+        self.reg_container.add_widget(data)
 
     def add_free_running(self, instance):
-        btn = Register(orientation='horizontal')
+        btn = Button(text='lol',size_hint_y=None, height=30)
         self.free_run_container.add_widget(btn)
 
 
@@ -80,26 +82,34 @@ class Register(BoxLayout):
     def __init__(self,**kwargs):
         super(Register, self).__init__(kwargs=kwargs)
 
-        name_layout = BoxLayout(orientation='horizontal')
-        value_layout = BoxLayout(orientation='horizontal')
+    def config(self):
+        self.name_label = Label(text='Nombre',size_hint=(0.25,None), height=30)
+        self.name_value = TextInput( size_hint=(0.25,None),heigth=30)
 
-        name_label = Label(text='Nombre\nreg')
-        self.name_value = TextInput()
+        self.values_label = Label(text='Valor', size_hint=(0.25,None), height=30)
+        self.values_value = TextInput(size_hint=(0.25,None), height=30)
 
-        name_layout.add_widget(name_label)
-        name_layout.add_widget(self.name_value)
-
-        values_label = Label(text='Valor\nreg')
-        self.values_value = TextInput()
-
-        value_layout.add_widget(values_label)
-        value_layout.add_widget(self.values_value)
-
-        self.add_widget(name_layout)
-        self.add_widget(value_layout)
+        self.add_widget(self.name_label)
+        self.add_widget(self.name_value)
+        self.add_widget(self.values_label)
+        self.add_widget(self.values_value)
 
     def get_name(self):
         return self.name_value._get_text()
 
     def get_value(self):
         return self.values_value._get_text()
+
+    def get_name_label(self):
+        return self.name_label
+
+    def get_name_input(self):
+        return self.name_value
+
+    def get_value_label(self):
+        return self.values_label
+
+    def get_value_input(self):
+        return self.values_value
+
+
--- omt/test.py
+++ omt/test.py
@@ -1,45 +1,27 @@
-from omt.controller.controller_starter import Coordinator
-from omt.controller.source.source_thread_function import SourceThread
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 
 
-import threading
-import time
+class GUIStart(App):
+    def build(self, **kwargs):
+        layout = GridLayout(cols=1, spacing=0, size_hint_y=None)
+        # Make sure the height is such that there is something to scroll.
+        layout.bind(minimum_height=layout.setter('height'))
+        root = ScrollView(size_hint=(None, None), size=(400, 400))
+        root.add_widget(layout)
 
+        for i in range(30):
+            btn = Button(text=str(i), size_hint=(0.5,None), height=20)
+            btn1 = Button(text=str(i), size_hint=(0.5,None), height=20)
+            lol = BoxLayout(orientation='horizontal',size_hint=(1, None), size=(1,20))
+            lol.add_widget(btn)
+            lol.add_widget(btn1)
+            layout.add_widget(lol)
 
-data_dic_s = {}
-data_dic_d = {}
+        return root
 
-'''
-
-        self.frec_init = data_dic['frec_init']
-        self.frec_end = data_dic['frec_end']
-        self.frec_step = data_dic['frec_step']
-
-        self.connection = telnetlib.Telnet(self.ip, self.port)
-
-        self.connection.write('power ' + data_dic['power'] + ' dbm\r\n')
-
-        print self.ask_a_command('power'), data_dic['power']
-'''
-souce_dic = {}
-data_dic_s = {}
-data_dic_d = {}
-data_dic_s['frec_init'] = 10000
-data_dic_s['frec_end']  = 1000000
-data_dic_s['frec_number_point'] =  101
-data_dic_s['power'] = '-6'
-data_dic_s['ip'] = '192.168.1.34'
-data_dic_s['port'] = '5023'
-
-data_dic_tone = {}
-data_dic_tone['ip'] = '192.168.1.34'
-data_dic_tone['port'] = '5023'
-data_dic_tone['frec'] = 123100
-data_dic_tone['power'] = '-6'
-
-#souce_dic['sweep'] = data_dic_s
-souce_dic['tone'] = [data_dic_tone,]
-controller = Coordinator(souce_dic,data_dic_d)
-controller.start()
-raw_input('hit me: ')
-controller.stop_the_process()
+GUIStart().run()
