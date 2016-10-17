00e521d55b0ac53b25bdae4e3fbc7add4010b690
amermelao <amermelao@gmail.com>
2016-10-17 12:24:17 -0300

    new main

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+This is the main of the controller to test de digital OMT.
+This is a develop made by Amermelao.
+
+Install:
+
+
+    sudo add-apt-repository ppa:kivy-team/kivy
+    sudo apt-get update
+    sudo apt-get install python-kivy
+
+"""
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+from omt.gui.init import GUIStart
+
+if __name__ == "__main__":
+    GUIStart().run()
